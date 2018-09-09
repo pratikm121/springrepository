c1113a622a89bcfd7e430d7456d3436bd9568897
pratikm121 <pratikm121@gmail.com>
2018-09-09 16:40:06 +0530

    Modiefied MyRestService for main url

--- MySpringBootApplication/src/main/java/com/ibm/spring/boot/rest/MyRestService.java
+++ MySpringBootApplication/src/main/java/com/ibm/spring/boot/rest/MyRestService.java
@@ -3,12 +3,14 @@ package com.ibm.spring.boot.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.spring.boot.model.Contacts;
 
-@RestController(value="test")
+@RestController
+@RequestMapping("/Users/")
 public class MyRestService {
 	
 	private final String GET_ALL_QUERY = "SELECT ID,NAME,MOBILE,MOBILE_2,HOME,OFFICE,EMAIL,HOME_ADDRESS,OFFICE_ADDRESS,CRTTMST FROM CONTACTS";
